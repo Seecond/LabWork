effd2543233e57b6396fccc8927aa9b0853e9f22
Seecond <43245839+Seecond@users.noreply.github.com>
2018-11-17 14:41:49 +0800

    2018.11.15上机作业

---
+++ 4.3/example4_03_Circle.cpp
@@ -0,0 +1,14 @@
+﻿//第2个文件:example4_03_Circle.cpp:表示圆类的实现
+
+#include"example4_03_Circle.h"   //包含头文件
+
+double Circle::Area()   //求圆的面积
+{
+	//PI = 3.14;
+	return PI * Radius*Radius;
+}
+
+double Circle::Circumference()  //求圆的周长
+{
+	return 2 * PI*Radius;
+}
---
+++ 4.4/main.cpp
@@ -0,0 +1,16 @@
+﻿//第3个文件:example4_04.cpp:定义主函数,定义对象并调用相应函数
+
+#include<iostream>  //包含系统文件
+using namespace std;
+#include"example4_04_Circle.h"   //包含自定义头文件
+
+int main()
+{
+	Circle c1(3.5), c2;   //定义类的两个对象,c2的半径用默认值
+	cout << "radius of c1=" << c1.GetRadius();   //调用常成员函数输出半径
+	cout << "area of c1=" << c1.Area() << ",cirumference of c1=" << c1.Circumference() << endl;
+	cout << "radius of c2=" << c2.GetRadius();   //调用常成员函数输出半径
+	cout << "area of c2=" << c2.Area() << ",cirumference of c2=" << c2.Circumference() << endl;
+	system("pause");
+	return 0;
+}
